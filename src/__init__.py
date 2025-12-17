77ab26787dda5265e5be09fc129e21d266d2c652
Janardhan Pulivarthi <j143@protonmail.com>
2025-12-17 07:56:07 +0530

    Initialize __init__.py for LDO simulation package

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""LDO Simulation Package - 5nm Digital Low-Dropout Regulator"""
+
+__version__ = '1.0.0'
+__author__ = 'j143'
+__doc__ = 'Complete Digital LDO simulation framework for AI accelerators'
